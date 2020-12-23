2bf796d66d8d8911080e43b5b4569a092d2ac4dc
marie-bisbo <marie@brainlabsdigital.com>
2020-12-23 15:29:42 +0000

    Add script to change camera after 5 seconds

---
+++ Horror Game/Assets/Scripts/Camera/FirstCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstCamera : MonoBehaviour
+{
+
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
+    public bool cameraIsOn = false;
+    public int cameraNumber;
+
+    void Start()
+    {
+        cameraNumber = 1;
+        StartCoroutine(SwitchCamera());
+    }
+
+    IEnumerator SwitchCamera ()
+    {
+        yield return new WaitForSeconds(5);
+        cameraTwo.SetActive(true);
+        cameraOne.SetActive(false);
+        cameraIsOn = true;
+        cameraNumber = 2;
+    }
+}
