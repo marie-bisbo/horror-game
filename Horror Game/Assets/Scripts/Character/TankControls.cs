28e1e0584bcf0514e1cb360bb1b27d2a2ae14705
marie-bisbo <marie@brainlabsdigital.com>
2020-12-26 16:32:15 +0000

    Add character movement

---
+++ Horror Game/Assets/Scripts/Character/TankControls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TankControls : MonoBehaviour
+{
+
+    public GameObject player;
+    public bool isMoving;
+
+    public float horizontalMovement;
+    public float verticalMovement;
+
+    public float rotationSpeed = 150f;
+    public float movementSpeed = 4f;
+        
+    void Update()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            isMoving = true;
+            player.GetComponent<Animator>().Play("Walk");
+            horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+            verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+            player.transform.Rotate(0f, horizontalMovement, 0f);
+            player.transform.Translate(0, 0, verticalMovement);
+        }
+        else
+        {
+            isMoving = false;
+            player.GetComponent<Animator>().Play("Idle");
+        }
+    }
+}
