bef26b691fa48e050b0997deda0d4efcdf43c4cf
marie-bisbo <marie@brainlabsdigital.com>
2020-12-30 11:48:18 +0000

    Add running to tank controls

--- Horror Game/Assets/Scripts/Character/TankControls.cs
+++ Horror Game/Assets/Scripts/Character/TankControls.cs
@@ -5,28 +5,49 @@ public class TankControls : MonoBehaviour
 
     public GameObject player;
     public bool isMoving;
+    public bool isRunning;
+    public bool backwardsMovementCheck = false;
 
     public float horizontalMovement;
     public float verticalMovement;
 
     public float rotationSpeed = 150f;
-    public float movementSpeed = 4f;
+    public float walkingSpeed = 4f;
+    public float runningSpeed = 15.1f;
         
     void Update()
     {
+        CheckIfRunning();
         if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
         {
             isMoving = true;
             if (Input.GetButton("SKey"))
             {
+                backwardsMovementCheck = true;
                 player.GetComponent<Animator>().Play("WalkBackwards");
             }
+
             else
             {
-            player.GetComponent<Animator>().Play("Walk");
+                backwardsMovementCheck = false;
+                if (isRunning == false)
+                {
+                    player.GetComponent<Animator>().Play("Walk");
+                }
+                else
+                {
+                    player.GetComponent<Animator>().Play("Run");
+                }
+            }
+            if (isRunning == false)
+            {
+                verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * walkingSpeed;
+            }
+            if (isRunning == true && backwardsMovementCheck == false)
+            {
+                verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * runningSpeed;
             }
             horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
-            verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
             player.transform.Rotate(0f, horizontalMovement, 0f);
             player.transform.Translate(0, 0, verticalMovement);
         }
@@ -36,4 +57,9 @@ public class TankControls : MonoBehaviour
             player.GetComponent<Animator>().Play("Idle");
         }
     }
+    void CheckIfRunning ()
+    {
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+    }
 }
+
