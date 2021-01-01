1171c0afb6511b275074bc410efaa72d3c216397
marie-bisbo <marie@brainlabsdigital.com>
2021-01-01 15:29:57 +0000

    Make zombie move towards player

---
+++ Horror Game/Assets/Scripts/Enemies/SimpleZombieMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleZombieMove : MonoBehaviour
+{
+
+    public GameObject destination;
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        navMeshAgent.SetDestination(destination.transform.position);
+    }
+}
